a34d8390355d307b90c5846f73dd80856e82c2de
user <raheemadebayorr@gmail.con>
2023-10-28 20:50:38 +0100

    created a custom useFetch component

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,35 +1,9 @@
-import { useState , useEffect} from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 
 const Homepage = () => {
-
-const [blogs, setBlogs] = useState(null)
-const [is_pending, setIs_pending] = useState(true)
-const [error, setError] = useState(null)
-
-useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-    .then(res => {
-        if(!res.ok){
-            throw Error("Cannot fetch data from the server")
-        }
-       return res.json()
-    })
-    .then(data => {
-        setBlogs(data)
-        setIs_pending(false)
-        setError(null)
-    }).catch(err => {
-        setError(err.message)
-        setIs_pending(false)
-    })
-}, [])
-
-// const handleDelete = (id) => {
-//     const newBlogs = blogs.filter(blog => blog.id !== id)
-//     setBlogs(newBlogs)
-// }
+const {error, is_pending, data: blogs} = useFetch("http://localhost:8000/blogs")
 
     return ( 
         <div className="home">
